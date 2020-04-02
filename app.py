db33611baccb84792de72499313838b507097876
Clinton-Davis <clintongadavis@gmail.com>
2020-04-02 11:39:12 +0100

    learned how to change ports and ip adress

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Clinton"
+    return "Hello, Clinton, your going great"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, port=5001)

1a8ccf36dba50dbedf77c17579b4ba007455d7f7
Clinton-Davis <clintongadavis@gmail.com>
2020-04-02 15:31:51 +0100

    laos pf new learnig

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from flask import Flask, render_template, url_for
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
-Bootstrap(app)
+
+#Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
+    cur = mysql.connection.cursor()
+    result_value = cur.execute("SELECT * FROM user")
+    if result_value > 0:
+        user = cur.fetchall()
+        return user[0]
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 
 if __name__ == "__main__":

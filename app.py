549f6a5b49c1286327c618dd48d2ebfed1573e7b
Clinton-Davis <clintongadavis@gmail.com>
2020-04-02 13:48:45 +0100

    added css booststrap

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
 
 @app.route('/')
 def index():
-    fruits= ['Apples', 'kiwi', 'Banana']
-    return render_template('index.html', fruits=fruits)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)

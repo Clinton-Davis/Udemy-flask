dcf9e1238d22777b91e74f9f4e40b5c80da5c8ed
Clinton-Davis <clintongadavis@gmail.com>
2020-04-03 11:37:21 +0100

    added navbar to page

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
+from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash, check_password_hash
 import yaml
+import os
 
 app = Flask(__name__)
+Bootstrap(app)
 
 #Configure db
 db = yaml.load(open('db.yaml'))
@@ -13,6 +17,8 @@ app.config['MYSQL_DB'] = db['mysql_db']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #This is we can use .name .age and not [0],[1]
 mysql = MySQL(app)
 
+app.config['SECRET_KEY'] = os.urandom(24)
+
 
 @app.route('/', methods=['GET', "POST"])
 def index():
@@ -21,6 +27,7 @@ def index():
         name = form['name']
         age = form['age']
         cur = mysql.connection.cursor()
+        # name = generate_password_hash(name) #This takes the password(name) and hashes it. 
         cur.execute("INSERT INTO employee(name, age) VALUES(%s, %s)", (name, age))
         mysql.connection.commit()
     return render_template('index.html')
@@ -32,6 +39,8 @@ def employees():
     result_value = cur.execute("SELECT * FROM employee")
     if result_value > 0:
         employees = cur.fetchall()
+        #check_password_hash(employees[1]['name'], '123456') # chck to see if password are same will boolen
+        session['username'] = employees[0]['name']
         return render_template('employees.html', employees=employees)
 
 if __name__ == "__main__":
